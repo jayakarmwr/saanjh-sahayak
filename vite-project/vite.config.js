69fbf00563b4995290b284ce13962337fb1f1133
jayakarmwr <jayakarmwr@gmail.com>
2024-10-16 22:23:04 +0530

    a

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build:{
-    outDir:'../backend/build'
+  build: {
+    outDir: 'frontend/dist', // Specify the output directory for the frontend build
+    // Add any other build options here as needed
   }
 })
